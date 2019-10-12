952a9151fbdf420d7288d77137abf254077bfe82
yue_zhang <yue_zhang.neu@neusoft.com>
2019-10-12 16:56:30 +0800

    [缓存问题]

--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -10,6 +10,8 @@
   import {get, host} from '../../utils'
 
   export default {
+    created () {
+    },
     name: 'detail',
     mounted () {
       this.goodsId = this.$root.$mp.query.id
@@ -25,13 +27,15 @@
     components: {},
     methods: {
       async getDetail () {
+        this.images = []
         const data = await get('/goods/' + this.goodsId)
         for (const image of data) {
           const url = host + '/image/' + image
           this.images.push(url)
         }
       }
-    }
+    },
+    computed: {}
   }
 </script>
 
