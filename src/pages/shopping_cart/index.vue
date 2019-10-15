a0c0cee0bd2a4db2676fb622aa978af0147b41a2
flyingV_zy <flyingV_zy@163.com>
2019-10-15 23:05:07 +0800

    '[cart]'

--- src/pages/shopping_cart/index.vue
+++ src/pages/shopping_cart/index.vue
@@ -1,15 +1,27 @@
 <template>
-    <div class="cart_page">
-        <div class="goods_in_cart" v-for="(goods, index) in cartInfo"
-             :style="{backgroundColor: index%2===0? color1: color2}">
-            <div class="goods_pic">
-                <img alt="" :src="goods.image">
-            </div>
-            <div class="goods-count">
-                <p>{{goods.count}}</p>
-            </div>
+  <div class="cart_page">
+    <div class="goods_in_cart" v-for="(goods, index) in cartInfo"
+         :style="{backgroundColor: index%2===0? color1: color2}">
+      <div class="goods_pic">
+        <img alt="" :src="goods.image">
+      </div>
+      <div class="goods-title">
+        <div class="title">
+          {{goods.title}}
         </div>
+        <div class="price_count">
+          <div class="price">{{'¥'+goods.price}}</div>
+          <div class="count-btn">
+            <div class="btn-border">
+              <div class="btn-1">-</div>
+              <div class="btn-2" :style="{color: index%2===0? color2: color1}">{{goods.count}}</div>
+              <div class="btn-3">+</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 
 </template>
 
@@ -55,7 +67,12 @@
       async getCart (openid) {
         const cart = await get('/cart/', {'openid': openid})
         for (const goods of cart) {
-          this.cartInfo.push({image: host + '/image/' + goods.image, count: goods.count})
+          this.cartInfo.push({
+            image: host + '/image/' + goods.image,
+            count: goods.count,
+            title: '懒人常备小白鞋清洁增白剂',
+            price: 14.9
+          })
         }
       }
     }
@@ -63,19 +80,73 @@
 </script>
 
 <style scoped>
-    .cart_page {
-        width: 100%;
-    }
+  .cart_page {
+    width: 100%;
+  }
 
-    .goods_in_cart {
-        height: 100px;
-        width: 100%;
-        padding: 10px;
-    }
+  .goods_in_cart {
+    height: 100px;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+  }
 
-    .goods_pic {
-        width: 80px;
-        height: 80px;
+  .goods_pic {
+    width: 80px;
+    height: 80px;
+    align-self: center;
+    margin-left: 10px;
+  }
 
-    }
+  .goods_pic img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .goods-title {
+    width: calc(100% - 90px)
+  }
+
+  .title {
+    height: 50%;
+    width: 100%;
+    text-align: center;
+    line-height: 50px;
+  }
+
+  .price_count {
+    height: 50%;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .price {
+    width: 40%;
+    height: 100%;
+    text-align: center;
+    line-height: 50px;
+  }
+
+  .count-btn {
+    width: 60%;
+    height: 100%;
+    text-align: center;
+  }
+
+  .btn-border {
+    width: 90px;
+    height: 30px;
+    border: solid 1px white;
+    display: flex;
+    flex-direction: row;
+    margin: 0 auto;
+  }
+
+  .btn-border div {
+    width: 30px;
+    height: 30px;
+    border: solid 1px white;
+    line-height: 30px;
+  }
 </style>
